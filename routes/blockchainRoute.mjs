4513abac2ff4352853150918dafdf0ea64de77c3
TenTimothy <saba@wmooragency.co>
2024-08-03 23:27:37 +0200

    Hash - done

--- routes/blockchainRoute.mjs
+++ routes/blockchainRoute.mjs
@@ -1,10 +1,17 @@
 import { Router } from 'express';
-import { getBlockchain } from '../controllers/blockchainControllers.mjs';
+import { getBlockchain, mineBlock } from '../controllers/blockchainControllers.mjs';
 
 const router = Router();
+
 router.get('/', (req, res) => {
     console.log('GET request received at /api/v1/blockchain');
     getBlockchain(req, res);
+});
+
+// Lägg till en route för att hantera POST-begäran för mining
+router.post('/mine', (req, res) => {
+    console.log('POST request received at /api/v1/blockchain/mine');
+    mineBlock(req, res);
 });
 
 export default router;
