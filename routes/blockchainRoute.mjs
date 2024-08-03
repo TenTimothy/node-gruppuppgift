2b3847ecf2e7fe1bca6ef2e8a353661a3c34a904
TenTimothy <saba@wmooragency.co>
2024-08-03 19:45:01 +0200

    Get blockchain - Done

--- routes/blockchainRoute.mjs
+++ routes/blockchainRoute.mjs
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import { getBlockchain } from '../controllers/blockchainControllers.mjs';
 
 const router = Router();
-
 router.get('/', (req, res) => {
     console.log('GET request received at /api/v1/blockchain');
     getBlockchain(req, res);
